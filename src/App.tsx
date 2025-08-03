0a29c22eed4363a20aec80bc4f227f17da6f6604
allanmosesfernandes <fernzallan14@gmail.com>
2025-08-03 15:25:33 +0100

    fd

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ const App = () => {
     useEffect(() => {
         const todosFromLocalStorage = localStorage.getItem('todos');
         console.log('first')
+        let hog = 'dfdf';
         if (todosFromLocalStorage) {
             setTodos(JSON.parse(todosFromLocalStorage));
         }
